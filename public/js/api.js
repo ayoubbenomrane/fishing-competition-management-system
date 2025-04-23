6148dc999895956222024bc0932647d1fc69e625
ayoubban2003 <ayoubbomrane@gmail.com>
2025-04-23 10:43:49 +0100

    updated api url

--- public/js/api.js
+++ public/js/api.js
@@ -1,4 +1,4 @@
-    const url = "http://localhost:3000/api/";
+    const url = "https://fathomless-garden-14159-487a779b2d94.herokuapp.com";
     async function apiRequest(endpoint, method = 'GET', body = null, contentType=null) {
         try {
             const options = {
